9a769f12f334fac45e30377ce2cbc8b3760a1966
Timothy Helton <timothy.j.helton@gmail.com>
2017-06-06 22:19:07 -0600

    ENH: add Utilities Module

---
+++ nyc_signature/utils.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""" Utilities Module
+
+.. moduleauthor:: Timothy Helton <timothy.j.helton@gmail.com>
+"""
+from matplotlib import pyplot as plt
+from matplotlib.ticker import FuncFormatter
+import seaborn as sns
+
+
+ax_formatter = {
+    'percent': FuncFormatter(lambda x, position: f'{x * 100:.1f}%'),
+    'thousands': FuncFormatter(lambda x, position: f'{x * 1e-3:.0f}'),
+}
+colors = sns.color_palette()
+size = {
+    'label': 14,
+    'legend': 12,
+    'title': 20,
+    'super_title': 24,
+}
+
+
+def save_fig(name=None, save=False):
+    """
+    Helper function to save or display figure.
+
+    :param str name: file name
+    :param bool save: if True the figure will be saved
+    """
+    if save:
+        plt.savefig(f'{name}.png', bbox_inches='tight',
+                    bbox_extra_artists=[size['super_title']])
+    else:
+        plt.show()
