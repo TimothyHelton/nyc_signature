757c76ba02bcfbec120eaf15653eedeb1e514776
Timothy Helton <timothy.j.helton@gmail.com>
2017-06-13 07:38:09 -0600

    ENH: add hospitals plot

--- nyc_signature/locations.py
+++ nyc_signature/locations.py
@@ -8,9 +8,13 @@
 import re
 import urllib
 
+from bokeh import io as bkio
+from bokeh import models as bkm
 import geocoder
 import pandas as pd
 import requests
+
+from . import keys
 
 
 class Hospitals:
@@ -18,6 +22,10 @@ class Hospitals:
     Class to identify and describe hospitals in New York City.
     """
     def __init__(self):
+        try:
+            self.api_key = keys.GOOGLE_API_KEY
+        except NameError:
+            self.api_key = None
         self.url = ('https://en.wikipedia.org/wiki/'
                     'List_of_hospitals_in_New_York_City')
         self.request = requests.get(self.url)
@@ -52,3 +60,50 @@ class Hospitals:
         self.hospitals = pd.DataFrame(hospitals,
                                       columns=['name', 'latitude',
                                                'longitude'])
+
+    def hospital_plot(self):
+        """
+        Plot hospital locations.
+
+        .. warning:: This method requires a Google API Key
+        """
+        map_options = {
+            'lat': 40.70,
+            'lng': -73.92,
+            'map_type': 'roadmap',
+            'zoom': 10,
+        }
+        plot = bkm.GMapPlot(
+            api_key=keys.GOOGLE_API_KEY,
+            x_range=bkm.Range1d(),
+            y_range=bkm.Range1d(),
+            map_options=bkm.GMapOptions(**map_options),
+            plot_width=400,
+            plot_height=600,
+        )
+        plot.title.text = 'New York City Hospitals'
+
+        hospital = bkm.Circle(
+            x='longitude',
+            y='latitude',
+            fill_alpha=0.8,
+            fill_color='#cd5b1b',
+            line_color=None,
+            size=14,
+        )
+
+        hospitals = bkm.sources.ColumnDataSource(self.hospitals)
+        plot.add_glyph(hospitals, hospital)
+
+        hover = bkm.HoverTool()
+        hover.tooltips = [
+            ('Location', '@name'),
+        ]
+        plot.add_tools(
+            hover,
+            bkm.PanTool(),
+            bkm.WheelZoomTool(),
+        )
+
+        bkio.output_file('hospitals.html')
+        bkio.show(plot)
