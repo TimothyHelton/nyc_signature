678f7dc3d717a48456896a89d7077846afe2f743
Timothy Helton <timothy.j.helton@gmail.com>
2017-06-13 20:14:06 -0600

    ENH: add stations plot

--- nyc_signature/subway.py
+++ nyc_signature/subway.py
@@ -108,30 +108,62 @@ class Stations:
             self.trains.loc[:, col] = (self.trains.loc[:, col]
                                        .astype('category'))
 
-    def train_plot(self, save=False):
+    def stations_plot(self):
         """
-        Plot subway stations by train.
+        Plot subway stations.
 
-        :param bool save: if True the figure will be saved
+        .. warning:: This method requires a Google API Key
         """
-        sns.lmplot(x='longitude', y='latitude',
-                   data=(self.trains
-                         .sort_values(by='train')),
-                   hue='train', fit_reg=False, legend=False, markers='d',
-                   scatter_kws={'alpha': 0.3}, size=10)
+        map_options = {
+            'lat': 40.70,
+            'lng': -73.92,
+            'map_type': 'roadmap',
+            'zoom': 10,
+        }
+        plot = bkm.GMapPlot(
+            api_key=self.api_key,
+            x_range=bkm.Range1d(),
+            y_range=bkm.Range1d(),
+            map_options=bkm.GMapOptions(**map_options),
+            plot_width=400,
+            plot_height=600,
+        )
+        plot.title.text = 'New York City Subway Stations'
 
-        legend = plt.legend(bbox_to_anchor=(1.10, 0.5), fancybox=True,
-                            fontsize=size['legend'], loc='center right',
-                            shadow=True, title='Train')
-        plt.setp(legend.get_title(), fontsize=size['label'])
-        plt.xlabel('Longitude', fontsize=size['label'])
-        plt.ylabel('Latitude', fontsize=size['label'])
+        subway = bkm.Diamond(
+            x='longitude',
+            y='latitude',
+            fill_color='#3062C8',
+            line_color=None,
+            size=10,
+        )
 
-        plt.tight_layout()
-        plt.suptitle('New York Subway Train Stations',
-                     fontsize=size['super_title'], y=1.03)
+        subway_stations = bkm.sources.ColumnDataSource(
+            data=(self.data
+                  .loc[:, ['name', 'latitude', 'longitude']]
+                  .join(self.data.loc[:, 'entrance_type']
+                        .astype(str))
+                  .join(self.data.loc[:, 'exit_only']
+                        .astype(str)
+                        .str.replace('nan', 'No'))
+                  .drop_duplicates())
+        )
+        plot.add_glyph(subway_stations, subway)
 
-        save_fig('stations_trains', save)
+        hover = bkm.HoverTool()
+        hover.tooltips = [
+            ('Location', '@name'),
+            ('Entrance Type', '@entrance_type'),
+            ('Exit Only', '@exit_only'),
+        ]
+        plot.add_tools(
+            hover,
+            bkm.PanTool(),
+            bkm.WheelZoomTool(),
+        )
+
+        bkio.output_file('stations.html')
+        bkio.show(plot)
 
     def stations_locations_plot(self):
         """
@@ -195,3 +227,28 @@ class Stations:
 
         bkio.output_file('stations_locations.html')
         bkio.show(plot)
+
+    def train_plot(self, save=False):
+        """
+        Plot subway stations by train.
+
+        :param bool save: if True the figure will be saved
+        """
+        sns.lmplot(x='longitude', y='latitude',
+                   data=(self.trains
+                         .sort_values(by='train')),
+                   hue='train', fit_reg=False, legend=False, markers='d',
+                   scatter_kws={'alpha': 0.3}, size=10)
+
+        legend = plt.legend(bbox_to_anchor=(1.10, 0.5), fancybox=True,
+                            fontsize=size['legend'], loc='center right',
+                            shadow=True, title='Train')
+        plt.setp(legend.get_title(), fontsize=size['label'])
+        plt.xlabel('Longitude', fontsize=size['label'])
+        plt.ylabel('Latitude', fontsize=size['label'])
+
+        plt.tight_layout()
+        plt.suptitle('New York Subway Train Stations',
+                     fontsize=size['super_title'], y=1.03)
+
+        save_fig('stations_trains', save)
